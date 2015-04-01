0b1e94f7bf5eef4c2f67216059ff6d86b8356be7
Benoit Moussaud <bmoussaud@xebialabs.com>
2015-04-01 13:44:41 +0200

    Moving to https://github.com/xebialabs-community/placeholders-mustachifier-maven-plugin

--- src/main/java/com/xebialabs/maven/mustache/provider/DefaultValueProvider.java
+++
@@ -1,9 +0,0 @@
-package com.xebialabs.maven.mustache.provider;
-
-public class DefaultValueProvider implements ValueProvider {
-
-    @Override
-    public String getValue(final String key, final String defaultValue) {
-        return defaultValue;
-    }
-}
--- src/main/java/com/xebialabs/maven/mustache/provider/ValueProvider.java
+++
@@ -1,7 +0,0 @@
-package com.xebialabs.maven.mustache.provider;
-
-
-public interface ValueProvider {
-
-    String getValue(String key, String defaultValue);
-}
